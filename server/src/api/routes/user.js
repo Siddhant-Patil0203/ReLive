7bcbb51e7ecfb056388732dbc97c1d23cd953ec3
Naresh <chandanbatven@gmail.com>
2023-10-13 01:37:13 +0530

    auth
    Signed-off-by: Naresh <chandanbatven@gmail.com>

--- server/src/api/routes/user.js
+++ server/src/api/routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express' 
 import { signup, signin, update } from '../controllers/user.js'
+import authuser from '../middlewares/authUser.js'
 
 const router = express.Router()
 
 router.post("/signup", signup)
 router.post("/signin", signin)
-router.put("/update", update)
+router.put("/update", authuser, update)
 export default router
