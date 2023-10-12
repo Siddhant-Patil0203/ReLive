ae9ef143ec4781a030d5f77a4831cc1d3bab2e15
Naresh <chandanbatven@gmail.com>
2023-10-12 20:07:27 +0530

    added email passord validation and user creation
    Signed-off-by: Naresh <chandanbatven@gmail.com>

--- server/src/api/controllers/user.js
+++ server/src/api/controllers/user.js
@@ -1,5 +1,5 @@
 import userModel from '../models/userModel.js'
-
+import bcrypt from 'bcrypt'
 
 
 /**
@@ -18,7 +18,42 @@ export const signup = async (req, res) => {
             })
         }
 
+
+        //check confirm password
+        if(password !== confirmPassword){
+            res.status(400).json({
+                msg: "confirm password doesn't match"
+            })
+        }
+
+        //password regex
+        const regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,0}$/
+
+        const emailDomains = [
+            "@gmail.com",
+            "@yahoo.com",
+            "@hotmail.com"
+        ]
+
+
+        //check password
+        if(!regex.test(password)){
+            return res.status(400).json({
+                msg: "password must contain at least one uppercase letter one lowercase letter one special character and one number"
+            })
+        }
+
+
+        //check email
+        if (!emailDomains.some((v) => email.indexOf(v) >= 0)) {
+            return res.status(404).json({
+                success: false,
+                msg: "Please enter a valid email address",
+            });
+        }
+
         
+
 
         //create user
         const newUser = await userModel.create({
