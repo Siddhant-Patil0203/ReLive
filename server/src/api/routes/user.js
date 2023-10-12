028dfc11d8d507fd2645651ab2afefe045dc7a59
Naresh <chandanbatven@gmail.com>
2023-10-13 00:31:25 +0530

    added onboarding
    Signed-off-by: Naresh <chandanbatven@gmail.com>

--- server/src/api/routes/user.js
+++ server/src/api/routes/user.js
@@ -1,8 +1,9 @@
 import express from 'express' 
-import { signup, signin } from '../controllers/user.js'
+import { signup, signin, update } from '../controllers/user.js'
 
 const router = express.Router()
 
 router.post("/signup", signup)
 router.post("/signin", signin)
+router.put("/update", update)
 export default router
